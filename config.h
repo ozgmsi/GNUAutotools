e0bf396bcfbc04e4322c508bbbefeccc3272442f
=Ozgur Durak <=>
2016-11-25 13:41:43 +0100

    Initial commit

---
+++ config.h
@@ -0,0 +1,26 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "helloprinter"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "ozgmsi@gmail.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "HelloPrinter"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "HelloPrinter 0.01"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "helloprinter"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.01"
+
+/* Version number of package */
+#define VERSION "0.01"
